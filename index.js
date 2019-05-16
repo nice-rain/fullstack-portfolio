d8ac4e03763310a6feef97d704d81996be3b1295
Walker Twyman <walker.twyman@bottlerocketstudios.com>
2019-05-16 16:24:20 -0500

    Setup dark theme

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 'use strict'
 
+//Global Variables - To be written to sessionstorage
+let isDarkTheme = false;
 
 //Called when we successfully send a message
 //Hides our entire form and displays a message that we sent our message
@@ -89,11 +91,80 @@ function bindRestoreFormLink()
     });
 }
 
+function setDarkTheme()
+{
+    //Change all of our css properties to dark ones
+    $('.fa-sun').hide();
+    $('.fa-moon').show();
+    
+
+    const rootElement = document.querySelector(':root')
+    const darkTheme = {
+        '--text-color': '#bbb',
+        '--bg-color': '#222',
+        '--heading-font-color': '#eee',
+        '--darker-section': '#292929'
+    }
+    for(let style in darkTheme) {
+        rootElement.style.setProperty(style, darkTheme[style])
+    }
+}
+
+function setLightTheme()
+{
+    //Change all of our css properties to light ones
+    const rootElement = document.querySelector(':root')
+    const lightTheme = {
+        '--text-color': '#777',
+        '--bg-color': '#FFF',
+        '--heading-font-color': '#111',
+        '--darker-section': '#eee'
+    }
+    for(let style in lightTheme) {
+        rootElement.style.setProperty(style, lightTheme[style])
+    }
+}
+
+//Function that will determine whether or not we should load a dark theme
+function loadColorTheme()
+{
+    //Fetch variable from localStorage
+
+    //if variable || prefers-color-scheme: dark
+
+        //Do dark theme logic here
+    
+    //if !variable || prefers-color-scheme: light
+    
+        //Set it to light theme here
+
+    // If we want, we can use a enum (rather than bool) for var
+    // If variable === 'CYCLE'
+        // if(isNotSpecified || hasNoSupport) {
+        //     console.log('You specified no preference for a color scheme or your browser does not support it. I Schedule dark mode during night time.')
+        //     now = new Date();
+        //     hour = now.getHours();
+        //     if (hour < 4 || hour >= 16) {
+        //     activateDarkMode();
+        //     }
+        // }
+}
+
+function bindChangeTheme()
+{
+    $('#js-night-mode').on("click", (event)=>{
+        console.log('clicked night mode button');
+        setDarkTheme();
+    });
+}
+
 //Function to load all click bindings on the page
 function loadBindings()
 {
     bindSubmit();
     bindRestoreFormLink();
+    loadColorTheme();
+    bindChangeTheme();
 }
 
 //Runs when page loads
