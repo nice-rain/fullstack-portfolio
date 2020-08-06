d33c8e072fb55048ee8777c37151c070303a7393
Walker Twyman <walk12288@gmail.com>
2020-08-06 15:33:47 -0500

    add tomato ML

---
+++ tomato-ml/precache-manifest.0d15382241885ef61faadba63924adfe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6857781c095dfb922a5570c02d0676c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba11ea42f90283cc99d9",
+    "url": "/static/css/main.6c112b4f.chunk.css"
+  },
+  {
+    "revision": "9f116d2c9f6b3fdc7752",
+    "url": "/static/js/2.1ea13db0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1ea13db0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba11ea42f90283cc99d9",
+    "url": "/static/js/main.40838b40.chunk.js"
+  },
+  {
+    "revision": "9101fc5917e53167e655",
+    "url": "/static/js/runtime-main.57577930.js"
+  }
+]);
