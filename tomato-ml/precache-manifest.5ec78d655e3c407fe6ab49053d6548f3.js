3473afd001e0d47effcf4c96b4cfa71fd5497ba1
Walker Twyman <walk12288@gmail.com>
2020-08-06 15:59:06 -0500

    fix bug with tomato-ml build

---
+++ tomato-ml/precache-manifest.5ec78d655e3c407fe6ab49053d6548f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9e9308c56416b28fcb50b5e700ff2d1",
+    "url": "/tomato-ml/index.html"
+  },
+  {
+    "revision": "6a5022258c8b39ae0e34",
+    "url": "/tomato-ml/static/css/main.6c112b4f.chunk.css"
+  },
+  {
+    "revision": "9f116d2c9f6b3fdc7752",
+    "url": "/tomato-ml/static/js/2.1ea13db0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tomato-ml/static/js/2.1ea13db0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a5022258c8b39ae0e34",
+    "url": "/tomato-ml/static/js/main.4f44274a.chunk.js"
+  },
+  {
+    "revision": "8194a5d4cf2c84123c99",
+    "url": "/tomato-ml/static/js/runtime-main.ae846d61.js"
+  }
+]);
