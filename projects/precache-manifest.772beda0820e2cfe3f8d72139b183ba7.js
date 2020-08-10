aeb946d8cc96f0b5ed300a058f65200871f33390
Walker Twyman <walk12288@gmail.com>
2020-08-10 14:28:21 -0500

    update projects folder

---
+++ projects/precache-manifest.772beda0820e2cfe3f8d72139b183ba7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8199c9feaf747b87f6671cb4209a0c3a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f666fa92e0215c84594",
+    "url": "/static/css/main.f4aaa93b.chunk.css"
+  },
+  {
+    "revision": "6f788e0126c98cbc9128",
+    "url": "/static/js/2.5e073a6b.chunk.js"
+  },
+  {
+    "revision": "5bd09eb7f2cf4d145d532d862be18942",
+    "url": "/static/js/2.5e073a6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f666fa92e0215c84594",
+    "url": "/static/js/main.bae4a263.chunk.js"
+  },
+  {
+    "revision": "c847fe93b499f2206cf8",
+    "url": "/static/js/runtime-main.7e794a24.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
