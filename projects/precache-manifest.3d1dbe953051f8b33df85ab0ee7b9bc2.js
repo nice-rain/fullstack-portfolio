91f2adb68d573fe700a5816355f56aaee2d0272b
Walker Twyman <walk12288@gmail.com>
2020-08-10 15:07:45 -0500

    change error handling to react app for website

--- projects/precache-manifest.3d1dbe953051f8b33df85ab0ee7b9bc2.js
+++ projects/precache-manifest.3d1dbe953051f8b33df85ab0ee7b9bc2.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "32d69750f9c760dd1667b37a5d0675d8",
+    "revision": "627211455420f7df94360b5a0fb3b21b",
     "url": "/projects/index.html"
   },
   {
-    "revision": "328ff4aed23893f7f448",
+    "revision": "909a83434d9266555d0c",
     "url": "/projects/static/css/main.f4aaa93b.chunk.css"
   },
   {
-    "revision": "d61764eb96c081288c63",
-    "url": "/projects/static/js/2.028bcfff.chunk.js"
+    "revision": "69094e2e2872c6f35caf",
+    "url": "/projects/static/js/2.9d09f773.chunk.js"
   },
   {
     "revision": "5bd09eb7f2cf4d145d532d862be18942",
-    "url": "/projects/static/js/2.028bcfff.chunk.js.LICENSE.txt"
+    "url": "/projects/static/js/2.9d09f773.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "328ff4aed23893f7f448",
-    "url": "/projects/static/js/main.91228bf0.chunk.js"
+    "revision": "909a83434d9266555d0c",
+    "url": "/projects/static/js/main.1763af03.chunk.js"
   },
   {
     "revision": "19599edd261711159f75",
--- projects/service-worker.js
+++ projects/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/projects/precache-manifest.63a65b8c10499791017f774af9780edb.js"
+  "/projects/precache-manifest.3d1dbe953051f8b33df85ab0ee7b9bc2.js"
 );
 
 self.addEventListener('message', (event) => {
